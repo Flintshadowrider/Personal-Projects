dacd93bdaa7f631c866a05193006f227f1beecac
BuildTools <unconfigured@null.spigotmc.org>
2015-10-20 17:26:32 -0700

    I've added a .bat file that speeds up a computer if it is being slow. It free's up RAM by ending various google chrome/steam tasks that may be running without the user's knowledge.

---
+++ End_Unnessesary_Processes.bat
@@ -0,0 +1,31 @@
+@echo off
+color 0e
+title End ALLL the processes!
+taskkill /F /IM Pen_TouchUser.exe
+taskkill /F /IM Pen_TabletUser.exe
+taskkill /F /IM YCMMirage.exe
+taskkill /F /IM BambooCore.exe
+taskkill /F /IM ONENOTEM.EXE
+taskkill /F /IM AdobeIPCBroker.exe
+taskkill /F /IM Adobe_CEF_Helper.exe
+taskkill /F /IM ASC.exe
+taskkill /F /IM armsvc.exe
+taskkill /F /IM ASCService.exe
+taskkill /F /IM ASCTray.exe
+taskkill /F /IM "Creative Cloud.exe"
+taskkill /F /IM chrome.exe
+taskkill /F /IM ChromeHelper.exe
+taskkill /F /IM DriverBooster.exe
+taskkill /F /IM notepad.exe
+taskkill /F /IM hamachi-2-ui.exe
+taskkill /F /IM RealTimeProtector.exe
+taskkill /F /IM SkyDrive.exe
+taskkill /F /IM Steam.exe
+taskkill /F /IM SteamService.exe
+taskkill /F /IM steamwebhelper.exe
+taskkill /F /IM Monitor.exe
+taskkill /F /IM SkyDrive.exe
+taskkill /F /IM SkypeC2CAutoUpdateSvc.exe
+taskkill /F /IM SkypeC2CPNRSvc.exe
+taskkill /F /IM EasyAntiCheat.exe
+pause
